da51fd0e755827a03807d2e9b65d8defd2ce6f7c
IvanEfremovW <Ivan.EfremovWM@yandex.ru>
2026-06-09 16:05:52 +0300

    feat: add VideoPipeline to orchestrate video processing

--- src/spark_detector/pipeline.py
+++ src/spark_detector/pipeline.py
@@ -2,9 +2,31 @@ from abc import ABC, abstractmethod
 
 from .video_source import BaseVideoSource
 from .output_handler import BaseOutputHandler
+from .detector import BaseSparkDetector
 
 
 class BasePipeline(ABC):
     @abstractmethod
     def process(self, source: BaseVideoSource, output_handler: BaseOutputHandler):
         pass
+
+
+class VideoPipeline:
+    def __init__(
+        self,
+        detector: BaseSparkDetector,
+    ):
+        self.detector = detector
+
+    def process(
+        self,
+        source: BaseVideoSource,
+        output_handler: BaseOutputHandler,
+    ):
+        try:
+            for frame in source.frames():
+                annotated_frame = self.detector.detect(frame)
+
+                output_handler.write(annotated_frame)
+        finally:
+            output_handler.close()
