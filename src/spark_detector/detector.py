5526ce259e163fb0405713800a20cf8b72c70cd0
IvanEfremovW <Ivan.EfremovWM@yandex.ru>
2026-06-09 15:27:31 +0300

    feat: add YOLO detector

--- src/spark_detector/detector.py
+++ src/spark_detector/detector.py
@@ -1,8 +1,30 @@
 from abc import ABC, abstractmethod
+import numpy as np
 from numpy.typing import NDArray
+from ultralytics import YOLO
 
 
 class BaseSparkDetector(ABC):
     @abstractmethod
     def detect(self, frame: NDArray) -> NDArray:
         pass
+
+
+class YOLOSparkDetector(BaseSparkDetector):
+    def __init__(self, model_path: str, device: int | str = "cuda"):
+        self.model = YOLO(model_path)
+        self.device = device
+
+        # Warm-up
+        dummy_frame = np.zeros((736, 736, 3), dtype=np.uint8)
+        self.model.predict(dummy_frame, verbose=False, save=False)
+
+    def detect(self, frame: NDArray) -> NDArray:
+        results = self.model.predict(
+            source=frame,
+            verbose=False,
+        )
+
+        annotated_frame = results[0].plot()
+
+        return annotated_frame
