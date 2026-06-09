b93990c593efa7cf062b61e471516b8db05abfa9
IvanEfremovW <Ivan.EfremovWM@yandex.ru>
2026-06-09 16:03:32 +0300

    feat: add VideoFileOutput handler that writes processed frames to a mp4 video file

--- src/spark_detector/output_handler.py
+++ src/spark_detector/output_handler.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from numpy.typing import NDArray
+import cv2
 
 
 class BaseOutputHandler(ABC):
@@ -10,3 +11,16 @@ class BaseOutputHandler(ABC):
     @abstractmethod
     def close(self):
         pass
+
+
+class VideoFileOutput(BaseOutputHandler):
+    def __init__(self, output_path: str, fps: float, frame_size: tuple):
+        self.output_path = output_path
+        fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # ty: ignore[unresolved-attribute]
+        self.writer = cv2.VideoWriter(output_path, fourcc, fps, frame_size)
+
+    def write(self, frame: NDArray):
+        self.writer.write(frame)
+
+    def close(self):
+        self.writer.release()
